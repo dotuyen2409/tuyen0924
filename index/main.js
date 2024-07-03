9a764c2e30fc90950a69400121e3cab7a558d0bf
dotuyen0924 <dotuyenx3@gmail.com>
2024-07-04 02:02:07 +0700

    add new

--- index/main.js
+++ index/main.js
@@ -1,10 +1,31 @@
-window.alert('Are you sure want to see?');
+// window.alert('Are you sure want to see?');
 
-let menuIcon = document.querySelector('#menu-icon');
+let menuIcon =document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-menuIcon.onclick = () =>{
+menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-}
+};
 
+
+
+
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => { 
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height =  sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*='+ id + ']').classList.add('active');
+            });
+        };
+    });
+};
